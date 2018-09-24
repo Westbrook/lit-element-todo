d6dda3375ea97c4d2b7198d485b691acb14fde35
Westbrook Johnson <westbrook.johnson@gmail.com>
2018-09-23 20:08:43 -0400

    tab space clean up. no fat arrors in templates

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,97 +5,100 @@ import './components/ToDoItem.js';
 // import Logo from './assets/logo.png';
 
 export class ToDo extends LitElement {
-    /**
-     * Declare the properties that will be
-     * available in the binding system
-     */
-    static get properties() {
-      return {
-        list: {type: Array},
-        todo: {type: String},
-      };
+  /**
+  * Declare the properties that will be
+  * available in the binding system
+  */
+  static get properties() {
+    return {
+      list: {type: Array},
+      todo: {type: String},
+    };
+  }
+  constructor() {
+    super();
+    this.list = [
+      this.todoItem('clean the house'),
+      this.todoItem('buy milk')
+    ];
+    this.todo = '';
+    this.createNewToDoItem = this.createNewToDoItem.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  todoItem(todo) {
+    return {todo}
+  }
+
+  createNewToDoItem() {
+    this.list = [
+      ...this.list,
+      this.todoItem(this.todo)
+    ];
+    this.todo = '';
+  }
+
+
+  handleKeyPress(e) {
+    if (e.target.value !== '') {
+      if (e.key === 'Enter') {
+        this.createNewToDoItem();
+      }
     }
-    constructor() {
-        super();
-        this.list = [
-            this.todoItem('clean the house'),
-            this.todoItem('buy milk')
-        ];
-        this.todo = '';
-    }
+  }
 
-    todoItem(todo) {
-      return {todo}
-    }
-
-    createNewToDoItem() {
-      this.list = [
-        ...this.list,
-        this.todoItem(this.todo)
-      ];
-      this.todo = '';
-    }
+  handleInput(e) {
+    this.todo = e.target.value;
+  }
 
 
-    handleKeyPress(e) {
-        if (e.target.value !== '') {
-          if (e.key === 'Enter') {
-            this.createNewToDoItem();
-          }
-        }
-    }
-
-    handleInput(e) {
-      this.todo = e.target.value;
-    }
+  // this is now being emitted back to the parent from the child component
+  deleteItem(indexToDelete) {
+    this.list = this.list.filter((toDo, index) => index !== indexToDelete);
+  }
 
 
-    // this is now being emitted back to the parent from the child component
-    deleteItem(indexToDelete) {
-      this.list = this.list.filter((toDo, index) => index !== indexToDelete);
-    }
+  render() {
+    return html`
+    ${style}
+    <div class="ToDo">
+      <h1>LitElement</h1>
+      <h1 class="ToDo-Header">LitElement To Do</h1>
+      <div class="ToDo-Container">
 
+        <div class="ToDo-Content">
 
-    render() {
-        return html`
-            ${style}
-            <div class="ToDo">
-                <h1>LitElement</h1>
-                <h1 class="ToDo-Header">LitElement To Do</h1>
-                <div class="ToDo-Container">
+          ${repeat(
+            this.list,
+            (item, key) => {
+              return html`
+                <to-do-item
+                  .item=${item.todo}
+                  .deleteItem=${this.deleteItem.bind(this, key)}
+                ></to-do-item>
+              `;
+            }
+          )}
+        </div>
 
-                    <div class="ToDo-Content">
+        <div>
+          <input
+            type="text"
+            .value=${this.todo}
+            @change=${this.handleInput}
+            @keypress=${this.handleKeyPress}
+          />
+          <button
+            class="ToDo-Add"
+            @click=${this.createNewToDoItem}
+          >+</button>
+        </div>
 
-                        ${repeat(
-                            this.list,
-                            (item, key) => {
-                                return html`
-                                  <to-do-item
-                                    .item=${item.todo}
-                                    .deleteItem=${this.deleteItem.bind(this, key)}
-                                  ></to-do-item>
-                                `;
-                            }
-                        )}
-                    </div>
-
-                    <div>
-                       <input
-                          type="text"
-                          .value=${this.todo}
-                          @change=${e => this.handleInput(e)}
-                          @keypress=${e => this.handleKeyPress(e)}
-                        />
-                       <button
-                          class="ToDo-Add"
-                          @click=${e => this.createNewToDoItem(e)}
-                        >+</button>
-                    </div>
-
-                </div>
-            </div>
-        `;
-    }
+      </div>
+    </div>
+    `;
+  }
 }
 
 customElements.define('to-do', ToDo);
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,28 +2,28 @@ import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import {style} from './ToDoItem-styles.js';
 
 export class ToDoItem extends LitElement {
-    /**
-     * Declare the properties that will be
-     * available in the binding system
-     */
-    static get properties() {
-      return {
-        item: {type: String},
-        deleteItem: {type: Function},
-      };
-    }
+  /**
+  * Declare the properties that will be
+  * available in the binding system
+  */
+  static get properties() {
+    return {
+      item: {type: String},
+      deleteItem: {type: Function},
+    };
+  }
 
-    render() {
-        return html`
-            ${style}
-            <div class="ToDoItem">
-                <p class="ToDoItem-Text">${this.item}</p>
-                <button class="ToDoItem-Delete"
-                     @click=${this.deleteItem}>-
-                </button>
-            </div>
-        `;
-    }
+  render() {
+    return html`
+    ${style}
+    <div class="ToDoItem">
+      <p class="ToDoItem-Text">${this.item}</p>
+      <button class="ToDoItem-Delete"
+        @click=${this.deleteItem}>-
+      </button>
+    </div>
+    `;
+  }
 }
 
 customElements.define('to-do-item', ToDoItem);
